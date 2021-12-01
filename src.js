166c01d0c88a21b05764d08e61cd573f409f8773
Clare Coates <clare.coates@dunelm.com>
2021-12-01 09:03:31 +0000

    initial work

---
+++ src.js
@@ -0,0 +1,12 @@
+'use strict'
+
+
+/* Take first input value as the start, if the next figure in the file is greater than previous then count, if less then ignore it
+
+1. Get first value then loop over the different values in the file
+2. Check to see if next value is higher
+3. If > then count and log increased
+4. If < then ignore and log decreased
+
+*/
+
