7a63e2c224ed0bc50970894e2a127f59196c3f0a
Clare Coates <clare.coates@dunelm.com>
2021-12-01 15:54:18 +0000

    Final code

---
+++ sonarSweep.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+
+const text = fs.readFileSync("./myfileofnumbers.txt", "utf-8");
+const textByLine = text.split("\n").map((i) => Number(i))
+
+console.log(textByLine);
+
+const countIncrease = function () {
+    let count = 0;
+    for (let i = 0; i < textByLine.length; i++) {
+        if (textByLine[i] < textByLine[i + 1]) {
+            count++;
+        } 
+    }
+    console.log(count);
+}
+
+const countThreeMeasurements = function () {
+    let count3 = 0;
+    for (let i = 0; i < textByLine.length; i++) {
+        let current = textByLine[i] + textByLine[i+1] + textByLine[i+2];
+        let next = textByLine[i+1] + textByLine[i+2] + textByLine[i+3];
+        if (current < next) {
+            count3++;
+        }
+    }
+    console.log(count3);
+}
+countIncrease();
+countThreeMeasurements();
